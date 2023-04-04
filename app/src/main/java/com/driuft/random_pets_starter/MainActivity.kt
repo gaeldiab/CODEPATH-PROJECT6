3352792fc1034a04bfcab82b36c9815f20f174f8
Gael <gaeldiab1@gmail.com>
2023-04-04 16:05:22 -0400

    random

--- app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
+++ app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
@@ -11,10 +11,14 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var petList: MutableList<String>
+    private lateinit var rvPets: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvPets = findViewById(R.id.pet_list)
+        petList = mutableListOf()
 
         getDogImageURL()
     }
@@ -22,9 +26,18 @@ class MainActivity : AppCompatActivity() {
     private fun getDogImageURL() {
         val client = AsyncHttpClient()
 
-        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("Dog Success", "$json")
+        client["https://dog.ceo/api/breed/shiba/images/random/20", object : JsonHttpResponseHandler()
+        {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON)
+            {
+                val petImageArray = json.jsonObject.getJSONArray("message")
+                for (i in 0 until petImageArray.length())
+                {
+                    petList.add(petImageArray.getString(i))
+                }
+                val adapter = PetAdapter(petList)
+                rvPets.adapter = adapter
+                rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
             }
 
             override fun onFailure(
---
+++ app/src/main/java/com/driuft/random_pets_starter/PetAdapter.kt
@@ -0,0 +1,36 @@
+package com.driuft.random_pets_starter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class PetAdapter (private val petList: List<String>):RecyclerView.Adapter<PetAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val petImage: ImageView
+
+        init {
+            // Find our RecyclerView item's ImageView for future use
+            petImage = view.findViewById(R.id.pet_image)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.pet_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = petList.size
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(holder.itemView)
+            .load(petList[position])
+            .centerCrop()
+            .into(holder.petImage)
+    }
+}
